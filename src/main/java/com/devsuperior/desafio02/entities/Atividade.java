3720d296eb6ce88cc06cbb4bc325594cb3774076
Marcelo Sousa <marcelovsousa22@gmail.com>
2025-07-23 21:50:26 -0300

    criado classe 'Participante' e implementado relacionamento muitos-para-muitos entre as classes 'Atividade' e 'Participante'

--- src/main/java/com/devsuperior/desafio02/entities/Atividade.java
+++ src/main/java/com/devsuperior/desafio02/entities/Atividade.java
@@ -9,6 +9,8 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -30,12 +32,16 @@ public class Atividade {
 	@OneToMany(mappedBy = "atividade")
 	private Set<Bloco> blocos = new HashSet<>();
 	
+	@ManyToMany
+	@JoinTable(name = "tb_atividade_participante", joinColumns = @JoinColumn(name = "atividade_id"),inverseJoinColumns = @JoinColumn(name = "participante_id"))
+	private Set<Participante> participantes = new HashSet<>();
+	
 	public Atividade() {
 	}
 
-	public Atividade(Integer id, String name, Double preco, Categoria categoria) {
+	public Atividade(Integer id, String nome, Double preco, Categoria categoria) {
 		this.id = id;
-		this.name = name;
+		this.name = nome;
 		this.preco = preco;
 		this.categoria = categoria;
 	}
@@ -74,6 +80,10 @@ public class Atividade {
 	
 	public Set<Bloco> getBlocos() {
 		return blocos;
+	}
+	
+	public Set<Participante> getParticipantes() {
+		return participantes;
 	}
 		
 	@Override
--- src/main/java/com/devsuperior/desafio02/entities/Participante.java
+++ src/main/java/com/devsuperior/desafio02/entities/Participante.java
@@ -9,29 +9,31 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "tb_categoria")
-public class Categoria {
+@Table(name = "tb_participante")
+public class Participante {
 	
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
+	private String nome;
 	
-	@Column(columnDefinition = "TEXT")
-	private String descricao;
+	@Column(unique = true)
+	private String email;
 	
-	@OneToMany(mappedBy = "categoria")
+	@ManyToMany(mappedBy = "participantes")
 	private Set<Atividade> atividades = new HashSet<>();
-		
-	public Categoria() {
+	
+	public Participante() {
 	}
 
-	public Categoria(Integer id, String descricao) {
+	public Participante(Integer id, String nome, String email) {
 		this.id = id;
-		this.descricao = descricao;
+		this.nome = nome;
+		this.email = email;
 	}
 
 	public Integer getId() {
@@ -42,12 +44,20 @@ public class Categoria {
 		this.id = id;
 	}
 
-	public String getDescricao() {
-		return descricao;
+	public String getNome() {
+		return nome;
 	}
 
-	public void setDescricao(String descricao) {
-		this.descricao = descricao;
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
 	}
 	
 	public Set<Atividade> getAtividades() {
@@ -67,7 +77,7 @@ public class Categoria {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Categoria other = (Categoria) obj;
+		Participante other = (Participante) obj;
 		return Objects.equals(id, other.id);
 	}
 }
